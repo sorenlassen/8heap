f6d62ba55093e8a34bd5840903fc00f9a30416db
Soren Lassen <sorenlassen@gmail.com>
2019-10-17 09:20:27 -0700

    throw bad_alloc if allocation fails in extend() or push()

--- H8.hpp
+++ H8.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <new>
 extern "C" {
 #include "h8.h"
 }
@@ -12,14 +13,25 @@ class H8 {
   H8() { heap_init(&h); }
   ~H8() { heap_clear(&h); }
   inline size_type size() const { return h.size; }
-  inline elem_type* extend(size_type n) { return heap_extend(&h, n); }
+  inline elem_type* extend(size_type n) {
+    elem_type* ptr = heap_extend(&h, n);
+    if (!ptr) throw_bad_alloc();
+    return ptr;
+  }
   inline void pull_up(elem_type b, size_type q) { heap_pull_up(&h, b, q); }
   inline void push_down(elem_type a, size_type p) { heap_push_down(&h, a, p); }
   inline void heapify() { heap_heapify(&h); }
-  inline bool push(elem_type b) { return heap_push(&h, b); }
+  inline void push(elem_type b) {
+    bool ok = heap_push(&h, b);
+    if (!ok) throw_bad_alloc();
+  }
   inline elem_type top() const { return heap_top(&h); }
   inline elem_type pop() { return heap_pop(&h); }
 
  private:
+  [[noreturn]] static inline void throw_bad_alloc() {
+    std::bad_alloc exception;
+    throw exception;
+  }
   heap h;
 };
