fa9847a03aeda96ca05a2bf1e0c347139587d466
Soren Lassen <sorenlassen@gmail.com>
2019-10-20 07:58:31 -0700

    rename benchmark

--- HeapBenchmark.cpp
+++ HeapBenchmark.cpp
@@ -1,7 +1,7 @@
 /*
    brew install folly
    gcc -g -std=c11 -msse4 -O2 -DNDEBUG -c h8.c &&
-   g++ -g -std=c++17 -msse4 -O2 -DNDEBUG -lfollybenchmark h8.o h8Benchmark.cpp
+   g++ -g -std=c++17 -msse4 -O2 -DNDEBUG -lfollybenchmark h8.o HeapBenchmark.cpp
 */
 
 #include <cstddef>
