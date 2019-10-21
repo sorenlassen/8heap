f794cb0b0302a1e27ec04cf8b0f11a1b7f68fa15
Soren Lassen <sorenlassen@gmail.com>
2019-10-20 20:09:40 -0700

    traling underscores in all field names for consistency

--- H8.hpp
+++ H8.hpp
@@ -11,38 +11,38 @@ class H8 {
   typedef uint16_t elem_type;
   typedef ::size_t size_type;
 
-  H8() { heap_init(&h); }
-  ~H8() { heap_clear(&h); }
+  H8() { heap_init(&h_); }
+  ~H8() { heap_clear(&h_); }
   H8(const H8&) = delete;
   H8& operator=(const H8&) = delete;
 
-  size_type size() const { return h.size; }
+  size_type size() const { return h_.size; }
   elem_type* extend(size_type n) {
-    elem_type* ptr = heap_extend(&h, n);
+    elem_type* ptr = heap_extend(&h_, n);
     if (!ptr) throw_bad_alloc();
     return ptr;
   }
   template<class InputIterator>
   void append(InputIterator begin, InputIterator end) {
-    elem_type* ptr = heap_extend(&h, std::distance(begin, end));
+    elem_type* ptr = heap_extend(&h_, std::distance(begin, end));
     std::copy(begin, end, ptr);
   }
-  void pull_up(elem_type b, size_type q) { heap_pull_up(&h, b, q); }
-  void push_down(elem_type a, size_type p) { heap_push_down(&h, a, p); }
-  void heapify() { heap_heapify(&h); }
+  void pull_up(elem_type b, size_type q) { heap_pull_up(&h_, b, q); }
+  void push_down(elem_type a, size_type p) { heap_push_down(&h_, a, p); }
+  void heapify() { heap_heapify(&h_); }
   void push(elem_type b) {
-    bool ok = heap_push(&h, b);
+    bool ok = heap_push(&h_, b);
     if (!ok) throw_bad_alloc();
   }
-  bool is_heap() const { return heap_is_heap(&h); }
-  elem_type top() const { return heap_top(&h); }
-  elem_type pop() { return heap_pop(&h); }
-  void clear() { heap_clear(&h); }
+  bool is_heap() const { return heap_is_heap(&h_); }
+  elem_type top() const { return heap_top(&h_); }
+  elem_type pop() { return heap_pop(&h_); }
+  void clear() { heap_clear(&h_); }
 
  private:
   [[noreturn]] static void throw_bad_alloc() {
     std::bad_alloc exception;
     throw exception;
   }
-  heap h;
+  heap h_;
 };
--- StdMinHeap.hpp
+++ StdMinHeap.hpp
@@ -17,44 +17,44 @@ class StdMinHeap {
   StdMinHeap(const StdMinHeap&) = delete;
   StdMinHeap& operator=(const StdMinHeap&) = delete;
 
-  size_type size() const { return array.size(); }
+  size_type size() const { return array_.size(); }
   elem_type* extend(size_type n) {
-    size_type old_size = array.size();
-    array.resize(old_size + n);
-    return &array[old_size];
+    size_type old_size = array_.size();
+    array_.resize(old_size + n);
+    return &array_[old_size];
   }
   template<class InputIterator>
   void append(InputIterator begin, InputIterator end) {
-    array.insert(array.end(), begin, end);
+    array_.insert(array_.end(), begin, end);
   }
   void pull_up(elem_type b, size_type q) { }
   void push_down(elem_type a, size_type p) { }
   void heapify() {
-    std::make_heap(array.begin(), array.end(), std::greater<elem_type>());
+    std::make_heap(array_.begin(), array_.end(), std::greater<elem_type>());
   }
   bool is_heap() const {
-    return std::is_heap(array.begin(), array.end(), std::greater<elem_type>());
+    return std::is_heap(array_.begin(), array_.end(), std::greater<elem_type>());
   }
   bool push(elem_type b) {
-    array.push_back(b);
-    std::push_heap(array.begin(), array.end(), std::greater<elem_type>());
+    array_.push_back(b);
+    std::push_heap(array_.begin(), array_.end(), std::greater<elem_type>());
     return true;
   }
   elem_type top() const {
     assert(size() > 0);
-    return array[0];
+    return array_[0];
   }
   elem_type pop() {
     elem_type a = top();
-    std::pop_heap(array.begin(), array.end(), std::greater<elem_type>());
-    array.pop_back();
+    std::pop_heap(array_.begin(), array_.end(), std::greater<elem_type>());
+    array_.pop_back();
     return a;
   }
   void clear() {
     std::vector<elem_type> empty;
-    array.swap(empty);
+    array_.swap(empty);
   }
 
  private:
-  std::vector<elem_type> array;
+  std::vector<elem_type> array_;
 };
