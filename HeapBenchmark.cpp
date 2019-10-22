71b8670d9bd15d101c1fe5ebd9e5d3a1fa97a3df
Soren Lassen <sorenlassen@gmail.com>
2019-10-21 18:55:06 -0700

    added sort benchmarks

--- HeapBenchmark.cpp
+++ HeapBenchmark.cpp
@@ -27,22 +27,31 @@ auto iter(CountType n, FunctionType f) -> decltype(auto) {
   return transform_iterator<FunctionType, counting_iterator<CountType>>(n, f);
 }
 
-template<class Heap>
-void fill(Heap& heap, typename Heap::size_type sz, bool ascending) {
-  typedef typename Heap::size_type size_type;
-  typedef typename Heap::elem_type elem_type;
+template<class Appendable>
+void fill(Appendable& out, typename Appendable::size_type sz, bool ascending) {
+  typedef typename Appendable::size_type size_type;
+  typedef typename Appendable::elem_type elem_type;
   elem_type max = std::numeric_limits<elem_type>::max();
   double mult = (max + 1.0) / sz;
   auto f = [=](size_type i) { return boost::numeric_cast<elem_type>(i * mult); };
   auto begin = iter(size_type(0), f);
   auto end = begin + sz;
-  heap.clear();
+  out.clear();
   if (ascending) {
-    heap.append(begin, end);
+    out.append(begin, end);
   } else {
-    heap.append(std::reverse_iterator(end), std::reverse_iterator(begin));
+    out.append(std::reverse_iterator(end), std::reverse_iterator(begin));
   }
 }
+
+// vector with added append() method
+struct AppendableVector : public std::vector<uint16_t> {
+  typedef value_type elem_type;
+  template<class InputIterator>
+  void append(InputIterator from, InputIterator to) {
+    insert(end(), from, to);
+  }
+};
 
 template<class Heap>
 void heapify(uint32_t n, size_t sz, bool ascending) {
@@ -59,12 +68,49 @@ void heapify(uint32_t n, size_t sz, bool ascending) {
   doNotOptimizeAway(x);
 }
 
+template<class Heap>
+void heapsort(uint32_t n, size_t sz, bool ascending) {
+  typedef typename Heap::elem_type elem_type;
+  Heap h;
+  std::vector<elem_type> result(sz);
+  for (int i = 0; i < n; ++i) {
+    BENCHMARK_SUSPEND {
+      fill(h, sz, ascending);
+    }
+    h.heapify();
+    for (size_t j = 0; j < sz; ++j) result[j] = h.pop();
+  }
+  doNotOptimizeAway(result[n % sz]);
+}
+
+void sort(uint32_t n, size_t sz, bool ascending) {
+  typedef typename AppendableVector::elem_type elem_type;
+  AppendableVector result;
+  for (int i = 0; i < n; ++i) {
+    BENCHMARK_SUSPEND {
+      fill(result, sz, ascending);
+    }
+    std::sort(result.begin(), result.end());
+  }
+  doNotOptimizeAway(result[n % sz]);
+}
+
 void heapify_h8_sorted(uint32_t n, size_t sz) { heapify<H8>(n, sz, true); }
 void heapify_h8_unsorted(uint32_t n, size_t sz) { heapify<H8>(n, sz, false); }
 void heapify_heap8_sorted(uint32_t n, size_t sz) { heapify<h8::Heap8>(n, sz, true); }
 void heapify_heap8_unsorted(uint32_t n, size_t sz) { heapify<h8::Heap8>(n, sz, false); }
 void heapify_std_sorted(uint32_t n, size_t sz) { heapify<StdMinHeap>(n, sz, true); }
 void heapify_std_unsorted(uint32_t n, size_t sz) { heapify<StdMinHeap>(n, sz, false); }
+
+void heapsort_h8_sorted(uint32_t n, size_t sz) { heapsort<H8>(n, sz, true); }
+void heapsort_h8_unsorted(uint32_t n, size_t sz) { heapsort<H8>(n, sz, false); }
+void heapsort_heap8_sorted(uint32_t n, size_t sz) { heapsort<h8::Heap8>(n, sz, true); }
+void heapsort_heap8_unsorted(uint32_t n, size_t sz) { heapsort<h8::Heap8>(n, sz, false); }
+void heapsort_std_sorted(uint32_t n, size_t sz) { heapsort<StdMinHeap>(n, sz, true); }
+void heapsort_std_unsorted(uint32_t n, size_t sz) { heapsort<StdMinHeap>(n, sz, false); }
+
+void sort_sorted(uint32_t n, size_t sz) { sort(n, sz, true); }
+void sort_unsorted(uint32_t n, size_t sz) { sort(n, sz, false); }
 
 } // namespace
 
@@ -86,6 +132,31 @@ BENCHMARK_RELATIVE_PARAM(heapify_std_unsorted, 100000)
 BENCHMARK_PARAM(heapify_h8_unsorted, 10000000)
 BENCHMARK_RELATIVE_PARAM(heapify_heap8_unsorted, 10000000)
 BENCHMARK_RELATIVE_PARAM(heapify_std_unsorted, 10000000)
+BENCHMARK_DRAW_LINE();
+BENCHMARK_PARAM(heapsort_h8_sorted, 1000)
+BENCHMARK_RELATIVE_PARAM(heapsort_heap8_sorted, 1000)
+BENCHMARK_RELATIVE_PARAM(heapsort_std_sorted, 1000)
+BENCHMARK_RELATIVE_PARAM(sort_sorted, 1000)
+BENCHMARK_PARAM(heapsort_h8_sorted, 100000)
+BENCHMARK_RELATIVE_PARAM(heapsort_heap8_sorted, 100000)
+BENCHMARK_RELATIVE_PARAM(heapsort_std_sorted, 100000)
+BENCHMARK_RELATIVE_PARAM(sort_sorted, 100000)
+BENCHMARK_PARAM(heapsort_h8_sorted, 10000000)
+BENCHMARK_RELATIVE_PARAM(heapsort_heap8_sorted, 10000000)
+BENCHMARK_RELATIVE_PARAM(heapsort_std_sorted, 10000000)
+BENCHMARK_RELATIVE_PARAM(sort_sorted, 10000000)
+BENCHMARK_PARAM(heapsort_h8_unsorted, 1000)
+BENCHMARK_RELATIVE_PARAM(heapsort_heap8_unsorted, 1000)
+BENCHMARK_RELATIVE_PARAM(heapsort_std_unsorted, 1000)
+BENCHMARK_RELATIVE_PARAM(sort_unsorted, 1000)
+BENCHMARK_PARAM(heapsort_h8_unsorted, 100000)
+BENCHMARK_RELATIVE_PARAM(heapsort_heap8_unsorted, 100000)
+BENCHMARK_RELATIVE_PARAM(heapsort_std_unsorted, 100000)
+BENCHMARK_RELATIVE_PARAM(sort_unsorted, 100000)
+BENCHMARK_PARAM(heapsort_h8_unsorted, 10000000)
+BENCHMARK_RELATIVE_PARAM(heapsort_heap8_unsorted, 10000000)
+BENCHMARK_RELATIVE_PARAM(heapsort_std_unsorted, 10000000)
+BENCHMARK_RELATIVE_PARAM(sort_unsorted, 10000000)
 
 int main(int argc, char** argv) {
   runBenchmarks();
